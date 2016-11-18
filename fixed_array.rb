1a6ba036acac31d541da254b2951aa91b14c6249
npentella <npentella@gmail.com>
2016-11-18 12:31:24 -0600

    Re-initialize repository to cover several problems

---
+++ fixed_array.rb
@@ -0,0 +1,31 @@
+require_relative 'errors'
+
+class FixedArray
+	attr_reader :size
+
+	def initialize(size)
+		@size = size
+		@elements = []
+	end
+
+	def set(index, element)
+		handle_out_of_bounds(index)
+		elements[index] = element
+	end
+
+	def get(index)
+		handle_out_of_bounds(index)
+		elements[index]
+	end
+
+	private
+	attr_reader :elements
+
+	def raise_out_of_bounds(index)
+		raise OutOfBoundsException.new("Index #{index} is out of bounds. Size is #{size}.")
+	end
+
+	def handle_out_of_bounds(index)
+		raise_out_of_bounds(index) if index >= size || index < 0		
+	end
+end
---
+++ fixed_array_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'fixed_array'
+
+describe FixedArray do
+	let(:fixed_array) {FixedArray.new(2)}
+
+  describe 'FixedArray#new' do
+  	it 'initializes with a given size' do
+  		expect(fixed_array.size).to eq(2)
+  	end
+  end
+  describe 'FixedArray#get(index)' do
+  	it 'gets value at specificed index' do
+  		fixed_array.set(0, 1)
+  		expect(fixed_array.get(0)).to eq(1)
+  	end
+  	it 'raises Out of Bounds exception if user tries to get a value outside the array' do
+  		expect {fixed_array.get(2)}.to raise_error(OutOfBoundsException)
+  	end
+  end
+  describe 'FixedArray#set(index, element)' do
+  	it 'sets element at specified index' do
+  		fixed_array.set(0, 1)
+  		expect(fixed_array.get(0)).to eq(1)
+  	end
+  	it 'raises Out of Bounds exception if user tries to get a value outside the array' do
+  		expect {fixed_array.set(2, 3)}.to raise_error(OutOfBoundsException)
+  	end
+  end
+end
