e88700861205e8f856a99b10dc957a58c4ecd1a5
npentella <npentella@gmail.com>
2016-12-06 14:14:22 -0600

    Write initialize test for Linked List class

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,5 +1,11 @@
 require_relative '../linked_list'
 
 describe LinkedList do
-	
+	let(:linked_list) { LinkedList.new }
+
+	describe 'LinkedList#new' do
+		it 'initializes with nil as the starting node' do
+			expect(linked_list.first_node).to eq(nil)
+		end
+	end
 end
