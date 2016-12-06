d087274d1f3003f91e7aa9dab2ed401fa0436a02
npentella <npentella@gmail.com>
2016-12-06 13:28:56 -0600

    Add initialization test for Node class

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,5 +1,12 @@
 require_relative '../node'
 
 describe Node do
-	
+	describe 'Node#new(element)' do
+		let(:element) {"string"}
+		let(:node) {Node.new(element)}
+		
+		it 'instantiates with given element' do
+			expect(node.element).to eq("string")
+		end
+	end	
 end
