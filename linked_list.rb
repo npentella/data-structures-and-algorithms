f8fae3ada6571a850aee2fb4920bf40b9bd7c124
npentella <npentella@gmail.com>
2016-12-06 15:32:50 -0600

    Add LinkedList#insert_first method, passign first associated test; Add LinkedList#get method, pass associated test

--- linked_list.rb
+++ linked_list.rb
@@ -1,8 +1,26 @@
+require_relative 'node'
+
 class LinkedList
 	attr_reader :first_node
 
 	def initialize
-		@length = nil
+		@length = 0
 		@first_node = nil
+	end
+
+	def insert_first(element)
+		@first_node = Node.new(element)
+		@length += 1
+	end
+
+	def get(index)
+		node_to_check = @first_node
+		(index + 1).times do |n|
+			if index == n
+				return node_to_check.value
+			else
+				node_to_check = node_to_check.next_node
+			end
+		end
 	end
 end
